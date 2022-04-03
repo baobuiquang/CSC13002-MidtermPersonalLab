ddfc35d75a021d483e1b8c466fb3a75b3f474755
Bui Quang Bao <quangbao.dev@gmail.com>
2022-04-03 20:02:13 +0700

    8. Rendering Data to Templates
    Rendering Data to Templates | Template Tags | Django (3.0) Crash Course Tutorials (pt 8)

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+
 # Create your views here.
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    return render(
+        request, 'accounts/dashboard.html',
+        {
+            # Objects
+            'orders': orders,
+            'customers': customers,
+            # Stat
+            'total_orders': orders.count(),
+            'delivered_orders': orders.filter(status='Delivered').count(),
+            'pending_orders': orders.filter(status='Pending').count(),
+            'total_customers': customers.count(),
+        }
+    )
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    return render(
+        request, 'accounts/products.html',
+        {
+            'products': products,
+        }
+    )
 
 def customer(request):
     return render(request, 'accounts/customer.html')
