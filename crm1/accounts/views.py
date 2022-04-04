ba523cfd8f3791730a3269e8f907cc4b70c19e60
Bui Quang Bao <quangbao.dev@gmail.com>
2022-04-04 20:41:13 +0700

    13. Register, Login, Logout, Authenticate
    User Registration and Login Authentication | Django (3.0) Crash Course Tutorials (pt 14)

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -1,12 +1,71 @@
+from operator import imod
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .models import *
 from .forms import *
 from .filters import *
 
 # Create your views here.
+
+def registerPage(request):
+
+    if not request.user.is_authenticated:
+
+        reg_form = CreateUserForm() # UserCreationForm(), new from forms.py
+
+        if request.method == 'POST':
+            reg_form = CreateUserForm(request.POST)
+            if reg_form.is_valid():
+                reg_form.save()
+                messages.success(request, 'Account was created successfully!')
+                return redirect('login')
+        # else:    
+        return render(
+            request, 'accounts/register.html',
+            {
+                'reg_form': reg_form,
+            }
+        )
+    
+    else:
+        return redirect('home')
+
+def loginPage(request):
+
+    if not request.user.is_authenticated:
+
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            
+            user = authenticate(request, username = username, password = password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username or password is incorrect!')
+        # else: 
+        return render(
+            request, 'accounts/login.html',
+            {
+
+            }
+        )
+    
+    else:
+        return redirect('home')
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
@@ -25,6 +84,7 @@ def home(request):
         }
     )
 
+@login_required(login_url='login')
 def products(request):
     products = Product.objects.all()
     return render(
@@ -34,6 +94,7 @@ def products(request):
         }
     )
 
+@login_required(login_url='login')
 def customer(request, customer_id):
     cstm = Customer.objects.get(id=customer_id)
     ords = cstm.order_set.all()
@@ -54,6 +115,7 @@ def customer(request, customer_id):
         }
     )
 
+@login_required(login_url='login')
 def createOrder(request):
 
     ord_form = OrderForm()
@@ -72,6 +134,7 @@ def createOrder(request):
             }
         )
 
+@login_required(login_url='login')
 def createOrderForACustomer(request, customer_id):
 
     OrderFormSet = inlineformset_factory(
@@ -108,6 +171,7 @@ def createOrderForACustomer(request, customer_id):
             }
         )
 
+@login_required(login_url='login')
 def updateOrder(request, order_id):
 
     ord = Order.objects.get(id = order_id)
@@ -127,6 +191,7 @@ def updateOrder(request, order_id):
             }
         )
 
+@login_required(login_url='login')
 def deleteOrder(request, order_id):
     ord = Order.objects.get(id = order_id)
     # ord_form = OrderForm(instance = ord)     # same as create order but pre-fill the info of this order
