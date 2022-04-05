54841286a722d4d61bb5b8063644d234a97d700a
Bui Quang Bao <quangbao.dev@gmail.com>
2022-04-05 08:47:18 +0700

    18. Password Reset (DISABLED for security reasons, because this repo is public)
    Password Reset Email | Django (3.0) Crash Course Tutorials (pt 20)

--- crm1/accounts/urls.py
+++ crm1/accounts/urls.py
@@ -1,4 +1,7 @@
+from re import template
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
@@ -11,6 +14,7 @@ urlpatterns = [
     path('user/', views.userPage, name='user-page'),
     path('account/', views.accountSettings, name='account'),
 
+
     path('products/', views.products, name='products'),
     path('customer/<str:customer_id>/', views.customer, name='customer'),
     
@@ -19,4 +23,22 @@ urlpatterns = [
     path('delete_order/<str:order_id>/', views.deleteOrder, name='delete_order'),
 
     path('create_order/<str:customer_id>/', views.createOrderForACustomer, name='create_order_for_a_customer'),
+
+
+
+    path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),
+     name="reset_password"),
+
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"), 
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"), 
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"), 
+        name="password_reset_complete"),
 ]
